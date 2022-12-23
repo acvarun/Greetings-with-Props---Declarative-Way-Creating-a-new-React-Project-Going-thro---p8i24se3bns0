fdc9197cb0c585fc031385ecfe731061aacf16d3
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-23 22:04:52 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+
+function Welcome({name}){
+    let a=`Hey ${name}!`
+    return (
+        <div>
+            <h1>{a}</h1>
+            <h2>Welcome to Newton School.</h2>
+        </div>
+    );
+}
+
+export default Welcome;
